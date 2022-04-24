43f890453db21aab393ef35f2edb7cb73a1daeb5
eo-lind <eo.lind@gmail.com>
2022-04-23 19:26:40 -0500

    added button and forms to create movie reviews. They work, but currently are saving reviews with a default userId until login functionality is added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,17 +5,18 @@ import { UserList } from "./user/UserList"
 import { MovieList } from "./movie/MovieList"
 import { PartyList } from "./party/PartyList"
 import { ReviewList } from "./review/ReviewList"
+import { ReviewForm } from "./review/ReviewForm"
 
 export const ApplicationViews = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />} />
-
                 {/* TODO Renders the user list - change to friends later */}
                 <Route path="/users" element={<UserList />} />
                 <Route path="/parties" element={<PartyList />} />
                 <Route path="/reviews" element={<ReviewList />} />
+                <Route path="/reviews/create" element={<ReviewForm />} />
                 <Route path="/movies" element={<MovieList />} />
             </Routes>
         </>
--- src/modules/ReviewManager.js
+++ src/modules/ReviewManager.js
@@ -5,10 +5,18 @@
 //     ).then((res) => res.json())
 // }
 
-
-
 export const getAllReviews = () => {
     return fetch(
         `http://localhost:8088/reviews?_expand=user&_expand=movie`
     ).then((res) => res.json())
+}
+
+export const addReview = (newReview) => {
+    return fetch(`http://localhost:8088/reviews`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newReview),
+    }).then((response) => response.json())
 }
