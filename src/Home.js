2dbd19ea8d3c0fb2a92ea60a6973f3a942a81beb
eo-lind <eo.lind@gmail.com>
2022-04-28 10:41:05 -0500

    css styling updates

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import { PartyListForHome } from "./components/party/PartyList";
 import { ReviewListForHome } from "./components/review/ReviewList";
 import { ShowLoggedInProfile } from "./components/user/UserList"
+import "./Home.css"
 
 export const Home = () => (
     <>
-      <main>
+      <section className="column-container">
           <div className="column"><PartyListForHome /></div>
           <div className="column"><ReviewListForHome /></div>
           <div className="column"><ShowLoggedInProfile /></div>
-      </main>
+      </section>
     </>
 )
